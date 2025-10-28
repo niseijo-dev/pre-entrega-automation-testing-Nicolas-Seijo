fb59eaffb9543cca28d9242756ea49dc9195b6e2
Nicolás Seijo <seijo.nicolas@outlook.com>
2025-10-28 20:54:32 -0300

    Cree la función 'test_catalogo' en test_saucedemo, para testear el catálogo, también se importan más cosas de Selenium y la función login de auxiliares.py

--- tests/test_saucedemo.py
+++ tests/test_saucedemo.py
@@ -1,6 +1,15 @@
+#Importo Pytest
 import pytest
+
+#Importo Selenium
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
+
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+from utils.auxiliares import login
 
 #Declaro la fixture con scope="function" para que se ejecute antes y después de cada test.
 @pytest.fixture(scope="function")
@@ -17,3 +26,22 @@ def driver():
     
     #Hago .quit() para cerrar todo en cada test.
     driver.quit()
+
+def test_catalogo(driver):
+    login(driver)
+    wait = WebDriverWait(driver, 10)
+
+    titleLocalizador = (By.CLASS_NAME, "title")
+    titleElemento = wait.until(EC.visibility_of_element_located(titleLocalizador))
+
+    assert titleElemento.text == "Products"
+
+    products = driver.find_elements(By.CLASS_NAME, "inventory_item")
+    assert len(products) > 0, "No se encontraron productos en la página"
+
+    nombrePrimerProd = driver.find_element(By.XPATH, "(//div[@class='inventory_item_name'])[1]").text
+    precioPrimerProd = driver.find_element(By.XPATH, "(//div[@class='inventory_item_price'])[1]").text
+
+    print ("PRIMER PRODUCTO:")
+    print("NOMBRE: ", nombrePrimerProd)
+    print("PRECIO: ", precioPrimerProd)
