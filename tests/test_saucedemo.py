4b6136aeb9a09c5c5e12468956139a4efb8fca94
Nicolás Seijo <seijo.nicolas@outlook.com>
2025-10-28 21:02:43 -0300

    Agregué mas comentarios en test_catalogo y realice el testeo del carrito en la funcion test_carrito, que también tiene comentarios descriptivos

--- tests/test_saucedemo.py
+++ tests/test_saucedemo.py
@@ -28,20 +28,56 @@ def driver():
     driver.quit()
 
 def test_catalogo(driver):
+    # Hago log in en la página y espero 10s
     login(driver)
     wait = WebDriverWait(driver, 10)
 
+    #Localizo y espero a que el titulo aparezca.
     titleLocalizador = (By.CLASS_NAME, "title")
     titleElemento = wait.until(EC.visibility_of_element_located(titleLocalizador))
 
+    # Verifico que el texto de 'title' sea el correcto
     assert titleElemento.text == "Products"
 
+    #Obtengo la cantidad de productos localizandolo como 'inventory_item' y verifico que no sean 0
     products = driver.find_elements(By.CLASS_NAME, "inventory_item")
     assert len(products) > 0, "No se encontraron productos en la página"
 
+    # Obtengo el nombre y precio del primer producto
     nombrePrimerProd = driver.find_element(By.XPATH, "(//div[@class='inventory_item_name'])[1]").text
     precioPrimerProd = driver.find_element(By.XPATH, "(//div[@class='inventory_item_price'])[1]").text
 
+    # Imprime el nombre y precio del primer producto
     print ("PRIMER PRODUCTO:")
     print("NOMBRE: ", nombrePrimerProd)
     print("PRECIO: ", precioPrimerProd)
+
+def test_carrito(driver):
+    #Logueo en la página
+    login(driver)
+
+    # Obtengo el nombre del primer producto
+    nombreProducto = driver.find_element(By.XPATH, "(//div[@class='inventory_item_name'])[1]").text
+    # Localizo el primer boton 'Add to cart' y le doy click
+    botonAgregar = driver.find_element(By.XPATH, "(//button[text()='Add to cart'])[1]")
+    botonAgregar.click()
+
+    # Espera de 10 segundos
+    wait = WebDriverWait(driver, 10)
+    #Localizo el indicador de productos del carrito y esperando a que aparezca obtengo el elemento
+    numeroCarritoLocalizador = (By.CLASS_NAME, "shopping_cart_badge")
+    numeroCarrito = wait.until(EC.visibility_of_element_located(numeroCarritoLocalizador))
+
+    #Verifico que el texto del indicador de cantidad de productos del carrito sea 1
+    assert numeroCarrito.text == "1", "El contador del carrito no es 1"
+
+    # Localizo el elemento 'Link' del Carrito, osea el botón para ir al carrito, y lo clickeo.
+    linkCarrito = driver.find_element(By.CLASS_NAME, "shopping_cart_link")
+    linkCarrito.click()
+
+    # Localizo el nombre del item del carrito y esperando a que aparezca extraigo su texto
+    itemCarritoLocalizador = (By.CLASS_NAME, "inventory_item_name")
+    itemCarrito = wait.until(EC.visibility_of_element_located(itemCarritoLocalizador)).text
+    
+    # Verifico que el nombre del producto en el carrito sea el mismo que el del catálogo
+    assert nombreProducto == itemCarrito, "El producto en el carrito no coincide" 
